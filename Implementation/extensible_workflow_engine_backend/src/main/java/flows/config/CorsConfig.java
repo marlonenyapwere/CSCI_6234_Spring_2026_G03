5832a97afcff4d4b7b9618829f2a7742887d49a6
marlonenyapwere <nyapweremars@gmail.com>
2026-04-14 12:35:07 -0400

    Added CORS config to allow client calls

---
+++ Implementation/extensible_workflow_engine_backend/src/main/java/flows/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package flows.config;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
