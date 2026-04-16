a602ae3eb3823cd370ecca1105cff0074ae1aa95
marlonenyapwere <nyapweremars@gmail.com>
2026-04-16 08:41:12 -0400

    documentanting REST endpoints for workflow  engine

--- Implementation/extensible_workflow_engine_backend/src/main/java/flows/WorkflowApiApplication.java
+++ Implementation/extensible_workflow_engine_backend/src/main/java/flows/WorkflowApiApplication.java
@@ -1,8 +1,33 @@
 package flows;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "flows")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Extensible Workflow Engine REST API Documentation",
+                description = "Extensible Workflow Engine REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Marlone Nyapwere",
+                        email = "marlone.nyapwere@gwu.edu",
+                        url = "marlonenyapwere@gwu.edu"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "marlonenyapwere@gwu.edu"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description =  "Extensible Workflow Engine REST API Documentation",
+                url = "https://www.marlonenyapwere.com/swagger-ui.html"
+        )
+)
 public class WorkflowApiApplication {
 
     public static void main(String[] args) {
--- Implementation/extensible_workflow_engine_backend/src/main/java/flows/api/WorkflowController.java
+++ Implementation/extensible_workflow_engine_backend/src/main/java/flows/api/WorkflowController.java
@@ -1,11 +1,23 @@
 package flows.api;
 
 import flows.definition.WorkflowDefinition;
+import flows.dto.ErrorResponseDto;
 import flows.runtime.WorkflowInstance;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+
+@Tag(
+        name = "Workflow REST APIs for managing Workflows",
+        description = "Workflow REST APIs to DEFINE workflows, CREATE workflow instance, QUERY execution states AND RESUME from user tasks"
+)
 @RestController
 @RequestMapping("/api")
 public class WorkflowController {
@@ -16,22 +28,99 @@ public class WorkflowController {
         this.workflowService = workflowService;
     }
 
+    @Operation(
+            summary = "REST endpoint to define a Workflow",
+            description = "Define and save your workflow. Obtain Workflow id to create an execution instance of the workflow"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/definition")
     public WorkflowDefinition createDefinition(@RequestBody WorkflowDefinition definition) {
         return workflowService.saveDefinition(definition);
     }
 
+
+    @Operation(
+            summary = "REST endpoint to create a Workflow instance to be executed",
+            description = "Create your workflow instance. Includes state persist."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/workflow/{definitionId}")
     public WorkflowInstance startWorkflow(@PathVariable String definitionId,
                                           @RequestBody(required = false) Map<String, Object> data) {
         return workflowService.startWorkflow(definitionId, data);
     }
 
+    @Operation(
+            summary = "REST endpoint to search workflow by instanceID",
+            description = "REST API to fetch workflow details based on a instance idr"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/workflow/{workflowId}")
     public WorkflowInstance getWorkflow(@PathVariable String workflowId) {
         return workflowService.getWorkflow(workflowId);
     }
 
+    @Operation(
+            summary = "Update Workflow execution based on a user action",
+            description = "REST API to resume workflow execution based on user task completion"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PutMapping("/workflow/{workflowId}/resume")
     public WorkflowInstance resumeWorkflow(@PathVariable String workflowId,
                                            @RequestBody ResumeRequest request) {
